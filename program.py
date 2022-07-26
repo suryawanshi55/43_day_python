6c5cad2e6171dc39a7a683e08e5c55ebb04ef858
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-26 17:26:11 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,66 @@
+# def nineam ():
+#     print('today is tuesday')
+# nineam()             today is tuesday
+
+# def student():
+#     print('I am a student')
+# student()                     I am a student
+
+# def hello(n):
+#     print('user not matching '+n)          #user not matching function
+# hello('function')
+
+# def hi(fname,lname):
+#     print('my name is',fname,'and my last name is',lname)
+# hi('pd','sd')                            # my name is pd and my last name is sd
+
+
+# def hey(k):
+#     print('function topic '+k)
+# hey('python')                            # function topic python
+# hey('adv python')                        # function topic adv python
+
+# def mobile(*m):
+#     print('mobile Brand',m)
+# mobile('Redmi','Realme','poco','vivo','oppo')    #mobile Brand ('Redmi', 'Realme', 'poco', 'vivo', 'oppo')
+# mobile(['Asus','moto','IQ','Nothing'])   # mobile Brand (['Asus', 'moto', 'IQ', 'Nothing'],)
+
+# def greet():
+#     print('hello')
+# greet()           # hello
+  
+
+# def greet(n):
+#     print('hello '+n)
+# greet('my')        hello my
+# greet('py')       hello py
+
+# def greet(*names):
+#     print('hey',names[0])
+# greet('python','programming','core','functions')       #hey python
+
+# def greet(*names):
+#     print('hey',names[3])
+# greet('python','programming','core','functions')   # hey functions
+
+# def details(n,a):
+#     print('hey '+n +'! your age is '+ str(a))
+# details('mine',99)               # hey mine! your age is 99
+# details(n='mine',a=99)    #hey mine! your age is 99
+# details(n='sd',a=99)         # hey sd! your age is 99
+
+# def details (n,a):
+#     print('hey ' +n + ' ! your age is' +str(a))
+# n=input('enter your name:')        #enter your name:nita
+# a=input('enter your age:')    #enter your age:34
+# details(n,a)              # hey nita ! your age is34
+
+# def r(x,y):
+#     return x+y
+# print(r,(5,6))           #<function r at 0x0000025E51F11480> (5, 6)
+
+# def z(p,q):
+#     return p*q,p+q;
+# p=int(input())
+# q=int(input())
+# print(z(p,q))
